db40884a00aa93249b16afaab458810c83679e71
Nothung429 <frank.gatto96.fg@gmail.com>
2022-04-27 14:26:24 +0200

    aggiunta html e javascript

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// chiedi all'utente il suo nome
+// chiedi all'utente il suo cognome
+// chiedi all'utente il suo colore preferito
+// scrivi sulla pagina il risultato aggiungendo 21 alla fine
+// risultato atteso nomecognomecolorepreferito21
