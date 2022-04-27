9f7aa2f01ed875fea40c73741b76d57aa107b5c6
Nothung429 <frank.gatto96.fg@gmail.com>
2022-04-27 15:20:49 +0200

    aggiunta script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
+// titolo pagina
+const title = document.getElementById("title").innerHTML
 // chiedi all'utente il suo nome
+const nome = prompt("Please enter your name");
+console.log(nome);
 // chiedi all'utente il suo cognome
+const cognome = prompt("Please enter your surname");
+console.log(cognome);
 // chiedi all'utente il suo colore preferito
-// scrivi sulla pagina il risultato aggiungendo 21 alla fine
+const colorepreferito = prompt("Please enter your favoritecolor");
+console.log(colorepreferito);
+// calcola il risultato aggiungendo 21 alla fine
+const somma = nome + cognome + colorepreferito + 21
+console.log(somma);
+// aggiungi alla pagina la password creata
+document.getElementById("title").innerHTML = `${title}, ecco la tua password: ${somma}`
 // risultato atteso nomecognomecolorepreferito21
